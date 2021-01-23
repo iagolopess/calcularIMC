4aadbd9920e85e9d782fe8f6fc110f1e67216d6f
iagolopess <iagoalopes@gmail.com>
2021-01-23 16:57:28 -0300

    novas features

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var botaoCalcula = document.querySelector("#calcula-imc")
 botaoCalcula.addEventListener("click", function(event){    
     event.preventDefault();
 
+    document.querySelector(".info-resultados").style.visibility = "visible";
+
     var peso = document.getElementById("peso").value.replace(",",".");
     var altura = document.getElementById("altura").value.replace(",",".");
 
@@ -11,12 +13,17 @@ botaoCalcula.addEventListener("click", function(event){
 });
 
 
-
-
 function calculaIMC(peso, altura){
+    var erro = document.querySelector(".msg-erro");
 
-    imc = peso / (altura * altura);
-    resultadoIMC(imc.toFixed(2))
+    if(peso <= 0 || altura >= 2.5){
+        erro.innerHTML = "Peso ou altura invalida"
+        
+    }else{
+        imc = peso / (altura * altura);
+        resultadoIMC(imc.toFixed(2))
+    }
+   
 
 }
 
@@ -27,23 +34,27 @@ function resultadoIMC(imc){
 
     if(imc < 18.5){
       txtResultado.innerHTML = `Seu IMC deu ${imc} isso siginifica que você está abaixo do ideal`
-
+      document.querySelector("#resultado").style.color = '#ff9100';
     } 
     
     if(imc >= 18.5 && imc <= 24.9){
         txtResultado.innerHTML = `Seu IMC deu ${imc} isso siginifica que você está no ideal`
+        document.querySelector("#resultado").style.color = '#099951';
     } 
     
     if(imc >= 25 && imc <= 29.9){
-        txtResultado.innerHTML = `Seu IMC deu ${imc} isso siginifica que você está com sobrepeso e precisa se cuidar mais`
+        txtResultado.innerHTML = `Seu IMC deu ${imc} isso siginifica que você está com sobrepeso e precisa se cuidar mais`;
+        document.querySelector("#resultado").style.color = '#d67900';
     } 
     
     if (imc >= 30 && imc <= 39.9){
         txtResultado.innerHTML = `Seu IMC deu ${imc} isso siginifica que você está em obesidade nivel 1, procure um médico`
+        document.querySelector("#resultado").style.color = '#d64e00';
     } 
     
     if(imc >= 40){
         txtResultado.innerHTML = `Seu IMC deu ${imc} isso siginifica que você está com obesidade grave`
+        document.querySelector("#resultado").style.color = '#d63900';
     }
 
 }
